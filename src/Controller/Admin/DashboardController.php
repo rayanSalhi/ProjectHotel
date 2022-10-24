3be83f59aac21697b486980ca170b6c9a3a5afd1
Siham21 <sihamdemarle@gmail.com>
2022-10-24 12:00:33 +0200

    maj reservation + BBD categorie+chambres

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -17,21 +17,6 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
 
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
         return $this->render('Admin/dashboard.html.twig');
     }
 
@@ -39,9 +24,9 @@ class DashboardController extends AbstractDashboardController
     {
         return Dashboard::new()
             ->setTitle('Projet Hotel V2');
-        // ->renderContentMaximize();
+  
     }
-
+ 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
@@ -51,6 +36,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToDashboard('Chambre libre', 'fa fa-key');
         yield MenuItem::linkToDashboard('Libérer une chambre', 'fa fa-clock');
         yield MenuItem::linkToDashboard('Modifier une réservation ', 'fa fa-edit');
-        yield MenuItem::linkToDashboard('Annulation d’une réservation ', 'fa fa-undo');
+        yield MenuItem::linkToDashboard('Annuler une réservation ', 'fa fa-undo');
     }
 }
--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CalendarRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ReservationController extends AbstractController
 {
-    #[Route('/reservation', name: 'app_reservation')]
-    public function index(): Response
+    #[Route('/home/reservation', name: 'reservation')]
+    public function reservation(): Response
     {
         return $this->render('reservation/reservation.html.twig', [
             'controller_name' => 'ReservationController',
+        ]);
+    }
+
+    #[Route('/no_reservation', name: 'no_reservation')]
+    public function nonReserver(): Response
+    {
+        return $this->render('reservation/no_reservation.html.twig', [
+            'controller_name' => 'No_ReservationController',
         ]);
     }
 }
